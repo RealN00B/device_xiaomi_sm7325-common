b05cde70887830f600444cabb787830f31dd5bd6
Cyber Knight <cyberknight755@gmail.com>
2022-06-17 02:12:27 +0800

    sm7325-common: Build KProfiles
    - Inherit KProfiles.
    - Address sepolicy.
    - Define sysfs_kprofiles as a domain.
    - Fix permission of nodes relevant to KProfiles at boot.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- common.mk
+++ common.mk
@@ -298,6 +298,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
+
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor \
